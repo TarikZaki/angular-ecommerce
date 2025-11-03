b963c924f10d72126af465f9cc50be4c82071343
TarikZaki <110928343+TarikZaki@users.noreply.github.com>
2025-11-03 18:01:28 +0200

    refactorNavbar (#12)

--- libs/ui/src/lib/components/navbar/navbar.ts
+++ libs/ui/src/lib/components/navbar/navbar.ts
@@ -13,4 +13,35 @@ export class Navbar {
   toggleMenu(): void {
     this.isMenuOpen = !this.isMenuOpen;
   }
+  mainLinks = [
+    { label: 'Home', path: 'home' },
+    { label: 'Products', path: 'products' },
+    { label: 'Categories', path: 'categories' },
+    { label: 'Brands', path: 'brands' },
+  ];
+
+  socialLinks = [
+    { icon: 'fab fa-instagram' },
+    { icon: 'fa-brands fa-facebook' },
+    { icon: 'fab fa-tiktok' },
+    { icon: 'fab fa-twitter' },
+    { icon: 'fab fa-linkedin' },
+    { icon: 'fab fa-youtube' },
+  ];
+
+  authLinksLoggedIn = [
+    { label: 'Cart', path: 'cart' },
+    { label: 'Signout', path: 'signout' },
+  ];
+
+  authLinksLoggedOut = [
+    { label: 'Login', path: 'login' },
+    { label: 'Register', path: 'register' },
+  ];
+
+  get authLinks() {
+    return this.isLoggedIn() ? this.authLinksLoggedIn : this.authLinksLoggedOut;
+  }
+
+  menuBars = [1, 2, 3];
 }
