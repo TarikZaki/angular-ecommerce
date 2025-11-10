933adc47c5d080e6cfee4d67b88da0384eb90378
TarikZaki <zakitarek20@gmail.com>
2025-11-10 13:41:16 +0200

    remove unused files and create guards

--- apps/nxPractice/src/app/app.ts
+++ apps/nxPractice/src/app/app.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { Button } from '@org/ui';
-import { Input } from '@org/ui';
 
 @Component({
-  imports: [RouterModule, Input, Button],
+  imports: [RouterModule],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
