7fe9c91ff9048a396c96c28f34763d63b8c4e2fa
TarikZaki <110928343+TarikZaki@users.noreply.github.com>
2025-11-26 16:08:42 +0200

    Add to card btn fire (#23)
    * Integrate ngx-toastr for toast notifications
    
    Added ngx-toastr and ngx-toastr-notifier dependencies and included toastr CSS in the project. Updated app configuration to provide Toastr support for toast notifications.
    
    * Refactor and centralize model interfaces
    
    Moved auth interfaces from libs/auth to libs/models for better reusability. Added new cart-related model interfaces and a shared common interface for Category and Subcategory. Updated product model to use shared interfaces and adjusted imports accordingly. Expanded index exports to include new models.
    
    * Add CartService with basic cart operations
    
    Introduces CartService to handle user cart retrieval and product addition via HTTP requests. Includes service implementation and a basic test to verify service creation. Also exports CartService from the services index.
    
    * Add cart functionality to product card and button
    
    Introduces an 'Add to cart' button on the product card with cart and toast services for feedback. Updates the reusable button component to emit a click event, allowing parent components to handle button actions.
    
    ---------
    
    Co-authored-by: Mostafa Ellethy <mostafa.ellethy93@gmail.com>

--- libs/productCard/src/lib/productCard/productCard.ts
+++ libs/productCard/src/lib/productCard/productCard.ts
@@ -1,4 +1,9 @@
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  input,
+} from '@angular/core';
 import {
   MatCard,
   MatCardActions,
@@ -9,7 +14,9 @@ import {
   MatCardTitle,
 } from '@angular/material/card';
 import { product } from '@org/models';
+import { CartService } from '@org/services';
 import { Button } from '@org/ui';
+import { ToastService } from 'ngx-toastr-notifier';
 
 /**
  *  * Product card component.
@@ -31,5 +38,22 @@ import { Button } from '@org/ui';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ProductCard {
+  private readonly cartService = inject(CartService);
+  private readonly toastrService = inject(ToastService);
   product = input.required<product>();
+
+  /**
+   *  Add product to cart
+   */
+  addToCart(id: string) {
+    this.cartService.AddProductToCart(id).subscribe({
+      next: () => {
+        this.toastrService.success('Product added to cart successfully');
+        // console.log(res);
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
 }
