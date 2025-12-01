5361a3d3d4f717cd772ec0f9866bd75c10eda363
TarikZaki <zakitarek20@gmail.com>
2025-12-01 13:23:38 +0200

    Add products-component Angular library
    Introduces a new 'products-component' library with Angular component, routing, unit tests, and configuration files. Integrates the products route into the main app and updates path mappings in tsconfig.base.json.

---
+++ libs/products-component/src/lib/products-component/products-component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-products-component',
+  imports: [],
+  templateUrl: './products-component.html',
+  styleUrl: './products-component.css',
+})
+export class ProductsComponent {}
