da9f4224007f9f36413f8e2d5889850395f51ca9
TarikZaki <zakitarek20@gmail.com>
2025-12-23 09:05:59 +0200

    Add empty cart state to item box component
    Introduces a user-friendly empty cart message and a call-to-action button in the item box component. Updates Angular imports to support the new button and router link functionality.

--- libs/cart/src/lib/item-box/item-box.ts
+++ libs/cart/src/lib/item-box/item-box.ts
@@ -1,15 +1,16 @@
 import { Component, input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { CartData } from '@org/models';
 import { ProductControls } from '@org/product-controls';
+import { Button } from '@org/ui';
 
 import { CartItems } from '../cart-items/cart-items';
-
 /**
  *
  */
 @Component({
   selector: 'lib-item-box',
-  imports: [CartItems, ProductControls],
+  imports: [CartItems, ProductControls, Button, RouterLink],
   templateUrl: './item-box.html',
   styleUrl: './item-box.css',
 })
